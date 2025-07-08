e5d4c420fb618e4f110a5be45af6012ccfb3f32b
556.vamsi@gmail.com <556.vamsi@gmail.com>
2025-07-08 20:19:16 +0100

    test-2

--- app/init.sh
+++ app/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pip install -r ./app/requirements.txt
+echo "PWD="$PWD
+pip install -r /app/requirements.txt
 alias 'll=ls -ltrah'
-mkdir -p ./app/home
-chmod 777 ./app/home
+mkdir -p /app/home
+chmod 777 /app/home
