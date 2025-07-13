a24d1cffa79d1d053b1e0756c847f4d619a6e425
556.vamsi@gmail.com <556.vamsi@gmail.com>
2025-07-13 14:46:22 +0100

    updated

--- app/init.sh
+++ app/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-get update && apt-get install -y curl
 # Wait until Spark master UI is reachable
 until curl -s http://localhost:8080 | grep -q "Spark Master"; do
   echo "Waiting for Spark to be ready..."
